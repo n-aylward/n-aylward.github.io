6a5b63279ea2832352642f0c01b9f0ac6dfacdee
Nick Aylward <nickolas.aylward@protonmail.com>
2026-09-18 11:30:25 -0400

    Remove blogging infra until I need it

--- astro.config.ts
+++ astro.config.ts
@@ -1,14 +1,6 @@
 import { defineConfig, envField, fontProviders } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import sitemap from "@astrojs/sitemap";
-import remarkToc from "remark-toc";
-import remarkCollapse from "remark-collapse";
-import {
-  transformerNotationDiff,
-  transformerNotationHighlight,
-  transformerNotationWordHighlight,
-} from "@shikijs/transformers";
-import { transformerFileName } from "./src/utils/transformers/fileName";
 import { SITE } from "./src/config";
 
 // https://astro.build/config
@@ -19,21 +11,6 @@ export default defineConfig({
       filter: page => SITE.showArchives || !page.endsWith("/archives"),
     }),
   ],
-  markdown: {
-    remarkPlugins: [remarkToc, [remarkCollapse, { test: "Table of contents" }]],
-    shikiConfig: {
-      // For more themes, visit https://shiki.style/themes
-      themes: { light: "min-light", dark: "night-owl" },
-      defaultColor: false,
-      wrap: false,
-      transformers: [
-        transformerFileName({ style: "v2", hideDot: false }),
-        transformerNotationHighlight(),
-        transformerNotationWordHighlight(),
-        transformerNotationDiff({ matchAlgorithm: "v3" }),
-      ],
-    },
-  },
   vite: {
     // eslint-disable-next-line
     // @ts-ignore
--- src/config.ts
+++ src/config.ts
@@ -5,16 +5,7 @@ export const SITE = {
   desc: "Resume site for Nick Aylward, a technical writer.",
   title: "Nick Aylward | Technical Writer",
   lightAndDarkMode: true,
-  postPerIndex: 4,
-  postPerPage: 4,
-  scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
   showArchives: false,
-  showBackButton: true,
-  editPost: {
-    enabled: false,
-    text: "Edit page",
-    url: "",
-  },
   dir: "ltr",
   lang: "en",
   timezone: "America/New_York",
